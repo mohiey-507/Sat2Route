c3cf7048d3dddae613db73e6cf75bd2bcdfcfbbf
mohiey <mohamedmohiey507@gmail.com>
2025-05-14 21:29:26 +0300

    Initial train.py

--- scripts/train.py
+++ scripts/train.py
@@ -1,5 +1,22 @@
+import os
 import sys
+import torch
+import argparse
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parent.parent))
-from sat2route import Generator, Discriminator, get_dataloaders
+
+from sat2route import (
+    Generator, Discriminator, Trainer, Loss,
+    get_dataloaders, default_config
+)
+
+def parse_args():
+    pass
+
+def main():
+    args = parse_args()
+    pass
+
+if __name__ == '__main__':
+    main()
