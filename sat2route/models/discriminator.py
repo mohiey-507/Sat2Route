ae75dc0532ada3e60eb400df3db46248eaf23b27
mohiey <mohamedmohiey507@gmail.com>
2025-08-22 14:33:30 +0300

    Apply spectral_norm to Discriminator

--- sat2route/models/discriminator.py
+++ sat2route/models/discriminator.py
@@ -1,22 +1,29 @@
 import torch
 import torch.nn as nn
 from .blocks import ContractBlock
+from torch.nn.utils import spectral_norm
 
 class Discriminator(nn.Module):
     """
     PatchGAN discriminator for image-to-image translation tasks
     Classifies if image patches are real or fake rather than the entire image
     """
-    def __init__(self, in_ch: int, hidden_ch: int = 32, depth: int = 4, norm_affine: bool = False):
+    def __init__(self, in_ch: int, hidden_ch: int = 32, depth: int = 4, use_dropout: bool = False, use_spectral: bool = True, dropout_rate: float = 0.5):
         super().__init__()
         
-        self.in_conv = nn.Conv2d(in_ch, hidden_ch, kernel_size=1)
+        in_conv = nn.Conv2d(in_ch, hidden_ch, kernel_size=1)
+        if use_spectral:
+            in_conv = spectral_norm(in_conv)
+        self.in_conv = in_conv
         ch = hidden_ch
         self.contracts = nn.ModuleList()
         for _ in range(depth):
-            self.contracts.append(ContractBlock(ch, use_norm=True, use_dropout=False, norm_affine=norm_affine))
+            self.contracts.append(ContractBlock(ch, use_dropout=use_dropout, use_spectral=use_spectral, activation='leaky', dropout_rate=dropout_rate))
             ch *= 2
-        self.out_conv = nn.Conv2d(ch, 1, kernel_size=1)
+        out_conv = nn.Conv2d(ch, 1, kernel_size=1)
+        if use_spectral:
+            out_conv = spectral_norm(out_conv)
+        self.out_conv = out_conv
     
     def forward(self, x, y):
         x = torch.cat([x, y], dim=1)
@@ -24,4 +31,3 @@ class Discriminator(nn.Module):
         for contract in self.contracts:
             x = contract(x)
         return self.out_conv(x)
-
